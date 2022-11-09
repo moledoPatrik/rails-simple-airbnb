68ef1dba90a4dc8bbcd6cd9102dfd3936943bda1
Patrik Moledo <moledop55@gmail.com>
2022-11-09 17:00:14 +0900

    unfinished simple airbnb exercise

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Destroying flats'
+
+Flat.destroy_all
+
+puts 'Creating flats'
+
+10.times do
+  Flat.create(
+    name: Faker::JapaneseMedia::OnePiece.island,
+    address: Faker::Address.street_address,
+    description: Faker::Lorem.paragraph(sentence_count: rand(2..6)),
+    price_per_night: rand(50..10_000),
+    number_of_guests: rand(1..6)
+  )
+end
+
+puts 'Finished'
